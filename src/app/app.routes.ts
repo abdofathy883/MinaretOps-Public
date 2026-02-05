a2678ee1674d57e860e18cc693971d5aef18c4b8
Abdelrahman Fathy <abdofathy883@gmail.com>
2026-02-05 13:57:47 +0200

    feat: add packages component with styles and HTML structure
    - Created a new Packages component with associated HTML and CSS files.
    - Implemented tab navigation for different service packages.
    - Added styles for buttons and background gradients.
    
    feat: implement footer layout and styles
    
    - Added Footer component with HTML and CSS for layout.
    - Included quick links and social media icons in the footer.
    
    feat: create header layout and styles
    
    - Developed Header component with navigation links and call-to-action button.
    - Styled header for better visibility and user interaction.
    
    feat: add About page with content and styles
    
    - Created About page with sections detailing company information and mission.
    - Implemented styles for layout and responsiveness.
    
    feat: add Contact page structure
    
    - Created Contact page with basic HTML structure.
    - Added CSS file for future styling.
    
    feat: implement Home page with sections and components
    
    - Developed Home page with hero section, service descriptions, and client issues.
    - Integrated Packages component into Home page for service offerings.
    - Added styles for various sections to enhance visual appeal.
    
    test: add unit tests for new components
    
    - Created unit tests for Packages, About, Contact, and Home components to ensure functionality.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./pages/home/home').then((m) => m.Home),
+  },
+  {
+    path: 'about-us',
+    loadComponent: () => import('./pages/about/about').then((m) => m.About)
+  },
+  {
+    path: 'contact-us',
+    loadComponent: () => import('./pages/contact/contact').then((m) => m.Contact)
+  }
+];
---
+++ src/app/pages/home/home.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Packages } from "../../components/packages/packages";
+
+@Component({
+  selector: 'app-home',
+  imports: [Packages],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+
+}
