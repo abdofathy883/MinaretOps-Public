09165095f5a966af4340eaa976d1e467e3f3d5c8
Abdelrahman Fathy <abdofathy883@gmail.com>
2026-03-26 01:49:57 +0200

    ??

--- src/app/core/resolvers/seo-resolver.ts
+++ src/app/core/resolvers/seo-resolver.ts
@@ -4,7 +4,7 @@ import { Seo } from '../../services/seo/seo';
 import { ISeoPage } from '../../models/seo/i-seo-page';
 import { LanguageService } from '../../services/languages/language.service';
 import { isPlatformBrowser } from '@angular/common';
-import { of } from 'rxjs';
+import { catchError, of, timeout } from 'rxjs';
 
 export const seoResolver: ResolveFn<ISeoPage | null> = (route, state) => {
   const platformId = inject(PLATFORM_ID);
@@ -20,5 +20,9 @@ export const seoResolver: ResolveFn<ISeoPage | null> = (route, state) => {
   const url = route.url.map(s => s.path).join('/');
   const currentRoute = url ? `/${url}` : '/';
   const cuurentLanguage = languageService.currentLang();
-  return seoService.getSeo(currentRoute, cuurentLanguage);
+  // return seoService.getSeo(currentRoute, cuurentLanguage);
+  return seoService.getSeo(currentRoute, cuurentLanguage).pipe(
+    timeout(3000), // give it 3 seconds max
+    catchError(() => of(null))
+  );
 };
